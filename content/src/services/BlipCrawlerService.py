c0445acc88a4024457a4a6b822506031d7398ae3
souzaMateus99 <mateussam.souza@gmail.com>
2021-03-26 17:10:06 -0300

    implement verify if user already exists on bot

--- content/src/services/BlipCrawlerService.py
+++ content/src/services/BlipCrawlerService.py
@@ -1,3 +1,4 @@
+from logging import error
 import re
 import time
 from selenium import webdriver
@@ -9,88 +10,98 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class BlipCrawlerService:
-    BLIP_LOGIN_URL = 'http://portal.blip.ai/'
-    MILLISECONDS_WAIT_ELEMENT_LOAD = 120
+    __BLIP_BASE_URL = 'https://{0}.blip.ai'
+    __BLIP_DEFAULT_ORGANIZATION = 'portal'
+    __MILLISECONDS_WAIT_ELEMENT_LOAD = 6000
+    __ONE_MINUTE_IN_SECONDS_VALUE = 60
     
     def __init__(self, user_login, users_insert, bots):
         self.user_login = user_login
         self.users_insert = users_insert
         self.bots = bots
-        self.driver = self.__driver_factory()
+        self.__driver = self.__driver_factory()
 
     def do_portal_login(self):
-        self.driver.get(self.BLIP_LOGIN_URL)
+        self.__driver.get(self.__BLIP_BASE_URL.format(self.__BLIP_DEFAULT_ORGANIZATION))
 
-        self.__find_element((By.NAME, 'Username'), self.MILLISECONDS_WAIT_ELEMENT_LOAD).send_keys(self.user_login['mail'])
-        password_elem = self.__find_element((By.NAME, 'Password'), self.MILLISECONDS_WAIT_ELEMENT_LOAD)
+        self.__find_element((By.NAME, 'Username'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD).send_keys(self.user_login['mail'])
+        password_elem = self.__find_element((By.NAME, 'Password'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD)
 
         password_elem.send_keys(self.user_login['password'])
         password_elem.send_keys(Keys.ENTER)
 
     def is_logged(self):
-        return True if self.driver.get_cookie('.AspNetCore.Antiforgery.w5W7x28NAIs') == None else False
+        return True if self.__driver.get_cookie('.AspNetCore.Antiforgery.w5W7x28NAIs') == None else False
 
     def has_access_in_bot(self, bot_identity):        
-        self.driver.get('https://portal.blip.ai/application')
+        organization_id = self.__get_organization_id(bot_identity)
 
-        self.__find_element((By.ID, 'applications'), self.MILLISECONDS_WAIT_ELEMENT_LOAD)
+        print(f'Search bot "{bot_identity}" in bot list ({organization_id})')
         
-        bots_elem = self.__find_elements((By.XPATH, '//div[2] //contact-list //contact //div //ng-include //a'), self.MILLISECONDS_WAIT_ELEMENT_LOAD)
-
-        print(f"Search bot '{bot_identity}' in user's bot list")
+        self.__driver.get(f'{self.__BLIP_BASE_URL.format(organization_id)}/application')
+        
+        bots_elem = self.__find_elements((By.XPATH, '//div[@id="applications"] //contact-list //contact //div[@class="contact animated fadeIn"] //ng-include //a'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD)
 
         for bot_elem in bots_elem:
             if bot_elem.get_attribute("href").find(bot_identity) >= 0:
-                print('Bot find')
                 return True
 
         return False
 
     def do_register_team_member(self, bot_identity):
-        print("------------------------------------------------------")
+        organization_id = self.__get_organization_id(bot_identity)
         
-        self.driver.get(f'https://portal.blip.ai/application/detail/{bot_identity}/team')
+        self.__driver.get(f'{self.__BLIP_BASE_URL.format(organization_id)}/application/detail/{bot_identity}/team')
         
         for user_insert in self.users_insert:
             user_insert_mail = user_insert['mail']
             user_insert_profile = user_insert['profile']
 
+            print("------------------------------------------------------")
+
             print(f'Bot Identity: {bot_identity} | User: {user_insert_mail}')
 
-            self.__find_element((By.XPATH, '//*[@id="main-content-area"] //div //page-header //div[1] //div[1] //div[1] //div[2] //custom-content //button'), self.MILLISECONDS_WAIT_ELEMENT_LOAD).click()
-
-            self.__find_element((By.NAME, 'email'), self.MILLISECONDS_WAIT_ELEMENT_LOAD).send_keys(user_insert_mail)
-
-            profile_elems = self.__find_elements((By.XPATH, '/html/body/div[7]/div[2]/div[2]/form/div[1]/div[3]/ul/li'), self.MILLISECONDS_WAIT_ELEMENT_LOAD)
-
-            profile_name = self.__get_user_profile(user_insert_profile)
-
-            if profile_name != 'Customizado' and profile_name != None:
-                print('User profile found')
-
-                user_profile_elem = self.__get_profile_elem(profile_elems, profile_name)
-
-                self.__move_to_element(user_profile_elem).click().perform()
-
-                save_button_element = self.__find_element((By.XPATH, '/html/body/div[7]/div[2]/div[2]/form/div[2]/button[2]'), self.MILLISECONDS_WAIT_ELEMENT_LOAD)
-
-                self.__move_to_element(save_button_element).click().perform()
-
-                inserted_message_elem = self.__find_element((By.XPATH, '/html/body/div[3]/div[2]/ul/li/div/span/span'), self.MILLISECONDS_WAIT_ELEMENT_LOAD)
-
-                if self.__ocurred_error_insert(inserted_message_elem):
-                    print('Ocurred error in insert')
-                else:
-                    print('Inserted user with success')
-
-                print("------------------------------------------------------")
+            if self.__is_user_registered(user_insert_mail):
+                print('User already registered in bot')
             else:
-                if profile_name == 'Customizado':
-                    print("Profile 'Customizado' wasn't implemented yet")
-                if profile_name == None:
-                    print("Profile not found")
+                self.__find_element((By.XPATH, '//div[@class="custom-header-content flex items-center justify-end ml5 tr w-100"] //custom-content //button'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD).click()
 
-    def __driver_factory(self):
+                self.__find_element((By.NAME, 'email'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD).send_keys(user_insert_mail)
+
+                profile_elems = self.__find_elements((By.XPATH, '//ul[@class="rz-ticks"] //li'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD)
+
+                profile_name = self.__get_user_profile(user_insert_profile)
+
+                if profile_name != 'Customizado' and profile_name != None:
+                    print('User profile found')
+
+                    user_profile_elem = self.__get_profile_elem(profile_elems, profile_name)
+
+                    self.__move_to_element(user_profile_elem).click().perform()
+
+                    save_button_element = self.__find_element((By.XPATH, '//div[@class="modal-footer mt4"] //button[@class="bp-btn bp-btn--bot bp-btn--small"]'), self.__MILLISECONDS_WAIT_ELEMENT_LOAD)
+
+                    self.__move_to_element(save_button_element).click().perform()
+
+                    time.sleep(self.__ONE_MINUTE_IN_SECONDS_VALUE)
+
+                    self.__driver.refresh()
+                    
+                    time.sleep(self.__ONE_MINUTE_IN_SECONDS_VALUE)
+
+                    if self.__is_user_registered(user_insert_mail):
+                        print('user inserted with success')
+                    else:
+                        print('Ocurred error in insert')
+                else:
+                    if profile_name == 'Customizado':
+                        print("Profile 'Customizado' wasn't implemented yet")
+                    if profile_name == None:
+                        print("Profile not found")
+
+            print("------------------------------------------------------")
+
+    def __driver_factory(self) -> webdriver.Chrome:
         driver_options = webdriver.ChromeOptions()
 
         driver_options.add_argument('--no-sandbox')
@@ -101,17 +112,20 @@ class BlipCrawlerService:
         return webdriver.Chrome(options=driver_options)
     
     def __find_element(self, locator, search_timeout = 30):    
-        return WebDriverWait(self.driver, search_timeout).until(
+        return WebDriverWait(self.__driver, search_timeout).until(
             EC.presence_of_element_located(locator)
         )
 
     def __find_elements(self, locator, search_timeout = 30):
-        return WebDriverWait(self.driver, search_timeout).until(
+        return WebDriverWait(self.__driver, search_timeout).until(
             EC.presence_of_all_elements_located(locator)
         )
 
+    def __find_elements_with_condition(self, condition, search_timeout = 30):
+        return WebDriverWait(self.__driver, search_timeout).until(condition)
+
     def __move_to_element(self, element):
-        return ActionChains(self.driver).move_to_element(element)
+        return ActionChains(self.__driver).move_to_element(element)
 
     def __get_profile_elem(self, profile_elems, profile):
         for elem in profile_elems:
@@ -134,5 +148,20 @@ class BlipCrawlerService:
 
         return None
 
-    def __ocurred_error_insert(self, element):
-        return element.text.find('problema') >= 0 or element.text.find('erro') >= 0
+    def __get_organization_id(self, bot) -> str:
+        if len(bot.split(':')) > 1:
+            return bot.split(':')[0]
+            
+        return self.__BLIP_DEFAULT_ORGANIZATION
+
+    def __is_user_registered(self, user):
+        users_registered_element = self.__find_elements_with_condition(
+            EC.visibility_of_all_elements_located((By.XPATH, '//div[@class="container"] //div[@class="row team-cards"] //div //a //card[@class="card--mini-card"] //div //div //div //div //span')),
+            self.__MILLISECONDS_WAIT_ELEMENT_LOAD
+        )
+        
+        for user_registered in users_registered_element:
+            if user_registered.text == user:
+                return True
+
+        return False
